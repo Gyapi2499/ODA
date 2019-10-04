82a29870fd67e88ff7307a7b6bff7a405c714709
Macskovity <ma.matyi@gmail.com>
2019-10-04 12:47:08 +0200

    kurzusok entitása

---
+++ ODA/src/main/java/hu/elte/issuetracker/entities/Course.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.elte.issuetracker.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ *
+ * @author Máté
+ */
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String description;
+    
+    @Column(nullable = false)
+    private User createUser;
+    
+    @Column(nullable = false)
+    private LocalDateTime createDate;
+    
+    @Column(nullable = false)
+    private User modifyUser;
+    
+    @Column(nullable = false)
+    private LocalDateTime modifyDate;
+    
+    @Column(nullable = false)
+    private String location;
+    
+    @Column(nullable = false)
+    private LocalDateTime date;
+    
+    @Column(nullable = false)
+    private LocalDateTime deadLine;
+    
+    @Column(nullable = false)
+    private int maxNumber;
+    
+    @Column(nullable = false)
+    private String image;
+        
+    @ManyToMany
+    @JoinTable (name = "APPLICANTS_TO_COURSE")
+    private List<User> applicants;
+    
+    @ManyToMany
+    @JoinTable (name = "TEACHERS_TO_COURSE")
+    private List<User> teachers;
+    
+
+}
